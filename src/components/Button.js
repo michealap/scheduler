99c86aedbc509dbd441b38453aac71d57558ec7b
Micheala Palmer <michealapalmer@gmail.com>
2022-01-18 15:19:17 -0500

    refactor button component and confirm all tests pass

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,15 +1,13 @@
 import React from "react";
-
 import "components/Button.scss";
+const classNames = require("classnames");
 
 export default function Button(props) {
-   let buttonClass = "button";
-   if (props.confirm) {
-    buttonClass += " button--confirm";
-   }
-   if (props.danger) {
-      buttonClass += " button--danger";
-   }
+   // add class to button based on props
+  const buttonClass = classNames("button", {
+   "button--confirm": props.confirm,
+   "button--danger": props.danger
+ });
    return <button className={buttonClass} onClick={props.onClick}
    disabled={props.disabled}>{props.children}</button>;
 }
