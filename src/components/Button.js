00a7f6d420bfe5c80bd2f3a70ecdeddcb08340f8
Micheala Palmer <michealapalmer@gmail.com>
2022-01-18 15:00:26 -0500

    add interactivity to the button component

--- src/components/Button.js
+++ src/components/Button.js
@@ -10,5 +10,6 @@ export default function Button(props) {
    if (props.danger) {
       buttonClass += " button--danger";
    }
-   return <button className={buttonClass}>{props.children}</button>;
+   return <button className={buttonClass} onClick={props.onClick}
+   disabled={props.disabled}>{props.children}</button>;
 }
