5eaf7cbb910d2c608dbbcd7d3bdbb9b57ed5a4aa
Micheala Palmer <michealapalmer@gmail.com>
2022-01-18 15:56:42 -0500

    add DayListItem component and import component into stories/index.js

---
+++ src/components/DayListItem.js
@@ -0,0 +1,33 @@
+import React from "react";
+import classNames from "classnames";
+import "components/DayListItem.scss";
+
+export default function DayListItem(props) {
+  // for displaying number of available interview spots
+  const checkSpots = function(spots) {
+    if (spots === 0) {
+      return "no spots remaining";
+    }
+
+    if (spots === 1) {
+      return "1 spot remaining";
+    } else return `${spots} spots remaining`;
+  };
+
+  // add class names based on props
+  const dayClass = classNames("day-list__item", {
+    "day-list__item--selected": props.selected,
+    "day-list__item--full": props.spots === 0
+  });
+
+  return (
+    <li
+      data-testid="day"
+      onClick={() => props.setDay(props.name)}
+      className={dayClass}
+    >
+      <h2 className="text--regular">{props.name}</h2>
+      <h3 className="text--light">{checkSpots(props.spots)}</h3>
+    </li>
+  );
+}
