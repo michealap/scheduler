8946fe2abe2740cfe792454039452b2f6cbe5fe8
Micheala Palmer <michealapalmer@gmail.com>
2022-01-19 18:01:17 -0500

    add Appointment component with grouping

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import "components/Application.scss";
 import DayList from "components/DayList";
+import Appointment from "components/Appointment";
 
 const days = [
   {
@@ -26,27 +27,28 @@ export default function Application(props) {
     <main className="layout">
       <section className="sidebar">
       <img
-  className="sidebar--centered"
-  src="images/logo.png"
-  alt="Interview Scheduler"
-/>
-<hr className="sidebar__separator sidebar--centered" />
-<nav className="sidebar__menu">
-<DayList
-  days={days}
-  value={day}
-  onChange={setDay}
-/>
+        className="sidebar--centered"
+        src="images/logo.png"
+        alt="Interview Scheduler"
+      />
+      <hr className="sidebar__separator sidebar--centered" />
+      <nav className="sidebar__menu">
+      <DayList
+        days={days}
+        value={day}
+        onChange={setDay}
+      />
 
-</nav>
-<img
-  className="sidebar__lhl sidebar--centered"
-  src="images/lhl.png"
-  alt="Lighthouse Labs"
-/>
+      </nav>
+      <img
+        className="sidebar__lhl sidebar--centered"
+        src="images/lhl.png"
+        alt="Lighthouse Labs"
+      />
       </section>
       <section className="schedule">
         {/* Replace this with the schedule elements during the "The Scheduler" activity. */}
+        <Appointment />
       </section>
     </main>
   );
--- stories/index.js
+++ stories/index.js
@@ -10,6 +10,7 @@ import DayListItem from "components/DayListItem";
 import DayList from "components/DayList";
 import InterviewerListItem from "components/InterviewerListItem";
 import InterviewerList from "components/InterviewerList";
+import Appointment from "components/Appointment/index.js";
 
 //Test Button component behaviour
 storiesOf("Button", module)
@@ -138,3 +139,11 @@ storiesOf("Button", module)
       onChange={action("setInterviewer")}
     />
   ));
+  //test Appointment component behaviour
+
+  storiesOf("Appointment", module)
+  .addParameters({
+    backgrounds: [{ name: "white", value: "#fff", default: true }]
+  })
+  .add("Appointment", () => <Appointment />)
+  .add("Appointment with Time", () => <Appointment time="12pm" />)
