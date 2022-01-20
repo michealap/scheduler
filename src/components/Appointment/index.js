35e71ef44ac79f4853cb4aedd188ccc79ff744f6
Micheala Palmer <michealapalmer@gmail.com>
2022-01-19 19:38:01 -0500

    update component with props

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -4,7 +4,7 @@ import "./styles.scss";
 export default function Appointment(props) {
   return (
     <article className="Appointment">
-      Appointment
+      {props.time}
     </article>
   )
 }
