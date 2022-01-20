ace6e307523fdf4f0789832044bc6902805e4e21
Micheala Palmer <michealapalmer@gmail.com>
2022-01-19 19:19:12 -0500

    add Show component and stories to test

--- stories/index.js
+++ stories/index.js
@@ -14,6 +14,7 @@ import InterviewerList from "components/InterviewerList";
 import Appointment from "components/Appointment/index";
 import Header from "components/Appointment/Header"
 import Empty from "components/Appointment/Empty"
+import Show from "components/Appointment/Show"
 
 //Test Button component behaviour
 storiesOf("Button", module)
@@ -152,3 +153,12 @@ storiesOf("Button", module)
   .add("Appointment with Time", () => <Appointment time="12pm" />)
   .add("Header", () => <Header time="12pm" />)
   .add("Empty", () => <Empty onAdd={action("onAdd")} />)
+  .add("Show", () => (
+    <Show
+      student="Lydia Miller-Jones"
+      interviewer={interviewer}
+      onEdit={action("onEdit")}
+      onDelete={action("onDelete")}
+      interview={{ student: "Lydia Miller-Jones", interviewer }}
+    />
+  ))
