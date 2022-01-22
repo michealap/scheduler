78e9e443fd4a22ce551695f763adb1b6717e0e8e
Micheala Palmer <michealapalmer@gmail.com>
2022-01-22 01:21:09 -0500

    import useEffect and axios for day requests

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,25 +1,8 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "components/Application.scss";
 import DayList from "components/DayList";
 import Appointment from "components/Appointment";
-
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5, 
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
 
 const appointments = [
   {
@@ -60,7 +43,7 @@ const appointments = [
   }
 ];
 export default function Application(props) {
-  const [day, setDay] = useState("Monday");
+  const [days, setDays] = useState([]);
   const schedule = appointments.map((appointment) => {
     return (
       <Appointment
@@ -69,6 +52,12 @@ export default function Application(props) {
       />
     );
   });
+
+  useEffect(() => {
+    axios.get("api/days").then(response => {
+      setDays(() => response.data);
+    });
+  }, [days]);
 
   return (
     <main className="layout">
@@ -82,8 +71,8 @@ export default function Application(props) {
       <nav className="sidebar__menu">
       <DayList
         days={days}
-        value={day}
-        onChange={setDay}
+        value={days}
+        onChange={setDays}
       />
 
       </nav>
