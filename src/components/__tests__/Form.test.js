9ba009adafac7903e85579663cdfce871f3aff5e
Micheala Palmer <michealapalmer@gmail.com>
2022-01-25 18:19:26 -0500

    remove getByAltText from import

--- src/components/__tests__/Form.test.js
+++ src/components/__tests__/Form.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { render, cleanup, getByAltText, fireEvent } from "@testing-library/react";
+import { render, cleanup, fireEvent } from "@testing-library/react";
 
 import Form from "components/Appointment/Form";
 
