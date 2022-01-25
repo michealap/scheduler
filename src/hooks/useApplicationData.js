36e5083b414f18b13e7360d88c27be9d3c2b290c
Micheala Palmer <michealapalmer@gmail.com>
2022-01-24 23:08:27 -0500

    create custom hook to separate concerns

---
+++ src/hooks/useApplicationData.js
@@ -0,0 +1,64 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+export default function useApplicationData() {
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    appointments: {},
+    interviewers: {}
+  });
+
+  const setDay = day => setState(prev => ({ ...prev, day }));
+  // update state and resolve promises whenever appointments are updated
+  useEffect(() => {
+    Promise.all([
+      axios.get("/api/days"),
+      axios.get("/api/appointments"),
+      axios.get("/api/interviewers")
+    ])
+    .then(all => {
+      setState(prev => ({
+        ...prev, 
+        days: all[0].data, 
+        appointments: all[1].data, 
+        interviewers: all[2].data}));
+    })
+  }, []);
+
+  
+  function bookInterview(id, interview) {
+    const appointment = {
+      ...state.appointments[id],
+      interview: { ...interview }
+    };
+    const appointments = {
+      ...state.appointments,
+      [id]: appointment
+    };
+
+    return (axios.put(`/api/appointments/${id}`, appointment)//push change to db
+      .then(() => {
+        setState({...state, appointments: appointments});
+      })
+    )
+  }
+  // updating the specific appointment state with null interview
+  function cancelInterview(id) {
+    const appointment = {
+      ...state.appointments[id],
+      interview: null
+    };
+    const appointments = {
+      ...state.appointments,
+      [id]: appointment
+    };
+    return (axios.delete(`/api/appointments/${id}`, appointment)
+      .then(() => {
+        setState({...state, appointments: appointments})
+      })
+    )
+  }
+
+  return { state, setDay, bookInterview, cancelInterview };
+}
